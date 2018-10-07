0a36872c32024572b27c91f504836489c4327fa7
erkkmohanty <er.kkmohanty@gmail.com>
2018-10-07 15:38:34 +0530

    fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine( Fibonacci(9));
+            Console.ReadLine();
+        }
+
+        static int Fibonacci(int num)
+        {
+            if (num <= 0)
+                return 0;
+            if (num == 1)
+                return 1;
+            return Fibonacci(num - 1) + Fibonacci(num - 2);
+        }
+    }
+}
